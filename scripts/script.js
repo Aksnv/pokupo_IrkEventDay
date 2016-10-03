8acb391224a66c2100daf92202532887f4d0389e
Yuri Aksenov <aksenov.mailbox@gmail.com>
2016-10-03 12:05:36 +0300

    Открытие и закрытие выпадающего меню реализовано на JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-alert("Hello!");
+var menu_hamburger = document.querySelector(".page-header__menu-hamburger");
+var button = document.querySelector(".page-header__menu-hamburger button");
+
+button.addEventListener("click", function(event) {
+  event.preventDefault();
+  menu_hamburger.classList.toggle("page-header__menu-hamburger--active");
+});
